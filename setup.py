cf6706bb237a98047e4c608f771acf1b9359fe83
titoghose <titoghose@gmail.com>
2019-04-12 09:44:21 +0800

    Altered directory structure

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyLogy-titoghose",
+    version="0.0.1",
+    author="Upamanyu Ghose",
+    author_email="titoghose@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/titoghose/PyLogy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        #"License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
